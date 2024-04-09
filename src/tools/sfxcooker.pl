0611e4d18c805f554748695b539102df65badbd9
Nick Faro <yux50000@hotmail.com>
2024-04-09 11:01:58 -0400

    WIP sfx

---
+++ src/tools/sfxcooker.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env swipl
+
+:- set_stream(user_output, type(binary)).
+
+:- consult(vgm).
+:- use_module(library(main)).
+:- initialization(main, main).
+
+opt_type(in_file, in_file, file).
+main([]) :-
+    argv_usage(debug).
+main(Argv) :-
+    argv_options(Argv, _, Opts),
+    member(in_file(InFile), Opts),
+
+    phrase_from_file(seq(Bytes), InFile, [type(binary)]),
+    phrase(vgm(_LoopOffset, Commands), Bytes, _),
+    writeln(user_error, "Done parsing"),!,
+
+    cook(Commands, CookedCommands),
+    writeln(user_error, "Done cooking"),!,
+
+    byteified_gb(CookedCommands, ByteifiedCommands),
+    maplist(put_byte, ByteifiedCommands),
+    print(user_error, ByteifiedCommands),
+    writeln(user_error, "Done!"),!.
